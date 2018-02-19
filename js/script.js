78ea8fac12f20e2903cf717babfdcd90a57126dd
Ronit Sinha <ronittheawesome@gmail.com>
2018-02-19 18:47:58 -0500

    Added more tweets

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ $(document).ready(function() {
 	//$('.col-sm-6').find( ".media" ).find(".media-body").find("p:contains('@realSelim')").parent().find('.media-heading').html('<strong>Shah Ismail I</strong> (@ShahIsmail) - DATE HERE');
 	$('.col-sm-6').find( ".media" ).find(".media-body").find(".media-heading:contains('Shah Ismail I')").parent().parent().find('img').attr('src','Shah_Ismail.jpg').attr('width', '64').attr('height', '64');
 
-
+	// Surround all words starting with "@" in <a> tags
 	$('.col-sm-6').find( ".media" ).find(".media-body").find("p").html (function (_, html) {
+		// using a regex to select whole words that start with @
 		return html.replace (/(\B\@\w+)/g, '<a href=\"\">$1</a>');
 	});
 });
