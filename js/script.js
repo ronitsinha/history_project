68524d817f0e8d969604d1c901b439596c8a0a7e
Ronit Sinha <ronittheawesome@gmail.com>
2018-02-20 22:48:59 -0500

    Wrapping up

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-	//$('.col-sm-6').find( ".media" ).find(".media-body").find("p:contains('@ShahIsmail')").parent().find('.media-heading').html('');
 	$('.col-sm-6').find( ".media" ).find(".media-body").find(".media-heading:contains('Sultan Selim I')").parent().parent().find('img').attr('src','Selim-I-cover.jpg').attr('width', '64').attr('height', '64');
 
-	//$('.col-sm-6').find( ".media" ).find(".media-body").find("p:contains('@realSelim')").parent().find('.media-heading').html('<strong>Shah Ismail I</strong> (@ShahIsmail) - DATE HERE');
 	$('.col-sm-6').find( ".media" ).find(".media-body").find(".media-heading:contains('Shah Ismail I')").parent().parent().find('img').attr('src','Shah_Ismail.jpg').attr('width', '64').attr('height', '64');
 
 	// Surround all words starting with "@" in <a> tags
